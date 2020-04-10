904f50583d99e93dab2408d472cbc52061b1b320
Igor Sudenko <sudenkoweb@gmail.com>
2020-04-11 01:09:24 +0300

    styled-mixins.js: refactored;

--- src/utils/styled-mixins.js
+++ src/utils/styled-mixins.js
@@ -1,2 +1,4 @@
-export const marginMixin = ({ m }) =>
+const marginMixin = ({ m }) =>
   m && (m instanceof Array ? `margin: ${m.map((num) => `${num}px`).join(' ')} !important ;` : `margin: ${m}px;`);
+
+export { marginMixin };
