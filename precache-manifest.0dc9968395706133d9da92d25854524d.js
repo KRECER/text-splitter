f97f01d568fee7c8f16a1767328dc77f13f2eedc
Igor Sudenko <sudenkoweb@gmail.com>
2020-04-10 23:06:03 +0300

    Updates

---
+++ precache-manifest.0dc9968395706133d9da92d25854524d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a747c2c5f1be0e8a7d8f8b292801936",
+    "url": "/text-splitter/index.html"
+  },
+  {
+    "revision": "b002041c11ad3e66f811",
+    "url": "/text-splitter/static/js/2.a31ce378.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/text-splitter/static/js/2.a31ce378.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0dd9c2619c4c24722f",
+    "url": "/text-splitter/static/js/main.eba619a9.chunk.js"
+  },
+  {
+    "revision": "0f81566df44d90894939",
+    "url": "/text-splitter/static/js/runtime-main.5e74baf1.js"
+  },
+  {
+    "revision": "791f52beed4d47b6d8cb8be090f85c35",
+    "url": "/text-splitter/static/media/mear.791f52be.png"
+  }
+]);
