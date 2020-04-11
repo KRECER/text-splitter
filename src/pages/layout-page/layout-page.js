55d9d55c67e82eee5b59dbd1f100cdfdcf20dc7d
Igor Sudenko <sudenkoweb@gmail.com>
2020-04-11 23:19:24 +0300

    layout-page: refactored;

--- src/pages/layout-page/layout-page.js
+++ src/pages/layout-page/layout-page.js
@@ -1,71 +1,16 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
 
 import MUIGrid from '@material-ui/core/Grid';
 import MUIContainer from '@material-ui/core/Container';
 import MUISnackbar from '@material-ui/core/Snackbar';
-import BorderClearIcon from '@material-ui/icons/BorderClear';
+import MUIBorderClearIcon from '@material-ui/icons/BorderClear';
 
-import bgIMG from './mear.png';
 import { getColumnsText, getFullText } from '@/redux/selectors';
+import { useOutsideClick } from '@/hooks/use-outside-click';
 import { ActionCreators } from '@/redux/reducer';
+import { Item, Row, TextArea, Wrap } from './styled-components';
 import AppButton from '@/components/atoms/app-button';
-import AppTextArea from '@/components/atoms/app-textarea';
-
-const useOutsideClick = (ref, callback) => {
-  const handleClick = (e) => {
-    if (ref.current && !ref.current.contains(e.target)) {
-      callback();
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleClick);
-
-    return () => {
-      document.removeEventListener('click', handleClick);
-    };
-  });
-};
-
-const TextArea = styled(AppTextArea)`
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  height: 100%;
-  background: #fff;
-
-  .MuiInputBase-root {
-    height: 100%;
-  }
-
-  .MuiInputBase-input {
-    height: 100%;
-  }
-`;
-
-const Wrap = styled.div`
-  height: 100vh;
-  padding: 100px 0;
-  background: url(${bgIMG}) no-repeat center;
-  background-size: cover;
-`;
-
-const Row = styled(MUIGrid)`
-  margin: 0 -4px;
-  margin-bottom: 25px;
-`;
-
-const Item = styled.div`
-  position: relative;
-  height: 100%;
-  padding: 15px;
-  background-color: rgba(255, 255, 255, 0.9);
-  box-shadow: 1px 1px 30px rgba(0, 0, 0, 0.5);
-`;
 
 const LayoutPage = () => {
   const [isOpenSnackBar, setIsOpenSnackBar] = useState(false);
@@ -108,7 +53,7 @@ const LayoutPage = () => {
         <AppButton
           variant="contained"
           color="primary"
-          startIcon={<BorderClearIcon />}
+          startIcon={<MUIBorderClearIcon />}
           onClick={() => handleChangeCountCol(num)}
           disabled={!!selectedCol}
         >
@@ -124,7 +69,7 @@ const LayoutPage = () => {
     );
 
     return (
-      <MUIGrid item xs={12} md={4} key={num} ref={columnRefs[num - 1]}>
+      <MUIGrid item xs={12} md={4} key={`column-${num}`} ref={columnRefs[num - 1]}>
         <Item onClick={() => handleClickColumn(num)}>
           {columnTexts[`column${num}`]}
           {selectedCol === num && textArea}
